9176a8b20796f99d64b5a9a0ffac6aac7d62616a
gotomars <751081818@qq.com>
2023-06-17 10:00:19 +0900

    add vmtranslator -03

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -112,7 +112,61 @@ def parse(command):
         return command_map[tokens[0]], tokens[1], tokens[2]
 
 def arithmetic(operation):
-    pass
+    """: returns lits of hack assembly instructions to perform operation on top 1 or 2 stack items"""
+    if operation in ['add','sub']:
+        op = '+' if operation == 'add' else '-1'
+        return [
+            f"// {operation}",
+            "@SP",
+            "AM=M-1",
+            "D=M",
+            "A=A-1",
+            f"M=M{op}D"
+        ]
+    elif operation in ['neg', 'not']:
+        op = '-' if operation == 'neg' else '!'
+        return [
+            f"// {operation}",
+            "@SP",
+            "A=M-1",
+            f"M={op}M"
+        ]
+    elif operation in ['eq', 'gt','lt']:
+        jump_op = {'op': 'JNE','gt':'JLE','lt':'JGE'}[operation]
+        global num_labeled
+        num_labeled += 1
+        return [
+            f"// {operation}",
+            "@SP",
+            "AM=M-1",
+            "D=M",
+            "A=A-1",
+            "D=M-D",
+            f"@Not{operation + str(num_labeled)}",
+            f"D; {jump_op}",
+            "@SP",
+            "A=M-1",
+            "M=-1",
+            f"@End{operation + str(num_labeled)}",
+            "0; JMP",
+            f"(Not{operation+str(num_labeled)})",
+            "@SP",
+            "A=M-1",
+            "M=0",
+            f"(End{operation + str(num_labeled)})"
+        ]
+    elif operation in ['and', 'or']:
+        op = '&' if operation == 'and' else '|'
+        return [
+            f"// {operation}",
+            "@SP",
+            "AM=M-1",
+            "D=M",
+            "A=A-1",
+            f"M=D{op}M"
+        ]
+    else:
+        return []
 
 def push(mem_seg,index):
     pass
@@ -121,7 +175,10 @@ def pop(mem_seg, index):
     pass
 
 def label(label_name,function_name):
-    pass
+    return [
+        f"// label {label_name}",
+        f"({function_name}${label_name})"
+    ]
 
 def init():
     asm_code = ["// init",
@@ -131,19 +188,108 @@ def init():
                 "M=D"]
 
 def function(function_name, n_vars):
-    pass
+    asm_code = [
+        f"// function {function_name} {n_vars}",
+        f"({function_name})"
+    ]
+    n_vars = int(n_vars)
+    if n_vars >= 1:
+        asm_code += ["@SP",
+                     "A=M",
+                     "M=0"]
+        for i in range(n_vars - 1):
+            asm_code += ["AD=A+1",
+                         "M=0"]
+        if n_vars >= 2:
+            asm_code += ["@SP",
+                         "M=D"]
+    return asm_code
 
 def call(callee, n_args, caller):
-    pass
+    global num_labeled
+    num_labeled += 1
+    return_address = f"{caller}$ret.{num_labeled}"
+    return [
+        f"// call {callee} {n_args}",
+        f"@{return_address}",
+        "D=A",
+        "@SP",
+        "A=M",
+        "M=D",
+        "D=A+1",
+        "@LCL",
+        "D=D+M",
+        "A=D-M",
+        "M=D-A",
+        "D=A+1",
+        "@ARG",
+        "D=D+M",
+        "A=D-M",
+        "M=D-A",
+        "D=A+1",
+        "@THIS",
+        "D=D+M",
+        "A=D-M",
+        "M=D-A",
+        "@SP",
+        "D=M",
+        f"@{n_args}",
+        "D=D-A",
+        "@ARG",
+        "M=D",
+        "@5",
+        "D=A",
+        "@SP",
+        "MD=M+D",
+        "@LCL",
+        "M=D",
+        f"@{callee}",
+        "0; JMP",
+        f"({return_address})"
+    ]
 
 def get_return():
-    pass
+    return [
+        "// return",
+        "@LCL",
+        "D=M",
+        "@5",
+        "A=D-A",
+        "D=M",
+        "@retAddr",
+        "M=D",
+        "@SP",
+        "A=M-1",
+        "D=M",
+        "@ARG",
+        "A=M",
+        "M=D",
+        "@ARG",
+        "D=M+1",
+        "@SP",
+        "M=D",
+        "@LCL",
+        "AM=M-1",
+        "D=M",
+        "@THAT"
+    ]
 
 def goto(label_name,function_name):
-    pass
+    return [
+        f"// goto {label_name}",
+        f"@{function_name}${label_name}",
+        "0; JMP"
+    ]
 
 def if_goto(label_name,function_name):
-    pass
+    return [
+        f"// if-goto {label_name}",
+        "@SP",
+        "AM=M-1",
+        "D=M",
+        f"@{function_name}${label_name}",
+        "D; JNE"
+    ]
 
 if __name__ == '__main__':
     if len(sys.argv) != 2 :
